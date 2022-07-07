18bcfd8c8f7aaad3acc1ba7765cec46c5cfad89f
aaronhance <ah@aaronhance.me>
2022-07-08 00:35:14 +0100

    added keras tuner

--- pre_train.py
+++ pre_train.py
@@ -27,56 +27,47 @@ import os
 import gc
 import cv2
 import pickle
+import keras_tuner as kt
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-  tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.experimental.set_memory_growth(gpu, True)
 
+  #build model
+def build_model(hp):
+    hp_filter_count_1 = hp.Choice('filter_count', values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    hp_kernel_size_1 = hp.Choice('kernel_size', values=[3, 5, 7, 9, 11, 13, 15])
 
-class DQN:
-    #learning rate
-    lr = 0.001
-    batch_size = 32
-    model = None
-    optimizer = keras.optimizers.Adam(lr=lr)
-    loss_func = keras.losses.categorical_crossentropy
-    state_size = (160, 160, 3)
-    action_size = 4
+    hp_filter_count_2 = hp.Choice('filter_count_2', values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    hp_kernel_size_2 = hp.Choice('kernel_size_2', values=[3, 5, 7, 9, 11, 13, 15])
 
+    hp_filter_count_3 = hp.Choice('filter_count_2', values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    hp_kernel_size_3 = hp.Choice('kernel_size_2', values=[3, 5, 7, 9, 11, 13, 15])
 
-    def __init__(self):
-        #build model
-        self.model = self.build_model()
-        #compile model
-        self.model.compile(loss="mse", optimizer=keras.optimizers.Adam(lr=self.lr), metrics=['accuracy'])
-        #load model if exists
+    #model
+    model = keras.Sequential()
+    #convolutional layers
+    model.add(keras.layers.Conv2D(hp_filter_count_1, (hp_kernel_size_1, hp_kernel_size_1), activation='relu', input_shape=(160, 160, 3)))
+    model.add(keras.layers.Conv2D(hp_filter_count_2, (hp_kernel_size_2, hp_kernel_size_2), activation='relu'))
+    model.add(keras.layers.Conv2D(hp_filter_count_3, (hp_kernel_size_3, hp_kernel_size_3), activation='relu'))
+    #max pooling layer
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    #flatten layer
+    model.add(keras.layers.Flatten())
+    #dense layer
+    model.add(keras.layers.Dense(16, activation='relu'))
+    #output layer
+    model.add(keras.layers.Dense(4, activation='softmax'))
+    #return model
+    model.compile(loss="mse", optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
 
-        #print model summary
-        self.model.summary()
-
-    #build model
-    def build_model(self):
-        #model
-        model = keras.Sequential()
-        #convolutional layers
-        model.add(keras.layers.Conv2D(8, (3, 3), activation='relu', input_shape=self.state_size))
-        model.add(keras.layers.Conv2D(16, (5, 5), activation='relu', input_shape=self.state_size))
-        model.add(keras.layers.Conv2D(16, (7, 7), activation='relu', input_shape=self.state_size))
-        #max pooling layer
-        model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-        #flatten layer
-        model.add(keras.layers.Flatten())
-        #dense layer
-        model.add(keras.layers.Dense(16, activation='relu'))
-        #output layer
-        model.add(keras.layers.Dense(self.action_size, activation='softmax'))
-        #return model
-        return model
+    return model
 
 
 data = pickle.load(open("samples.pickle", "rb"))
 
 data = np.array(data)
+data = data[:int(len(data)/2)]
 
 x = np.array([i['screen'] for i in data], dtype=np.float32)
 y = np.array([i['keys'] for i in data], dtype=np.float32)
@@ -91,11 +82,17 @@ print(y[-1].shape)
 
 print(y[520:590])
 
-dqn = DQN()
+tuner = kt.tuners.Hyperband(build_model, max_epochs=2, factor=3, objective='val_accuracy')
+stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 
-model = dqn.model
+tuner.search(x, y, epochs=2, validation_split=0.1, callbacks=[stop_early])
 
-model.fit(x, y, batch_size=dqn.batch_size, epochs=1, verbose=1, validation_split=0.1)
+best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+print(best_hps)
+best_model = tuner.get_best_models(1)[0]
+
+# model = dqn.model
+# model.fit(x, y, batch_size=dqn.batch_size, epochs=1, verbose=1, validation_split=0.1)
 
 #save model weights
-model.save_weights('v3model.h5')
+best_model.save_weights('v3model.h5')
